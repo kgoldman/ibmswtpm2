46715e4c0cc47097c33be2d227aec7a66f5eef6f
Michal Suchanek <msuchanek@suse.de>
2020-09-01 14:42:13 +0200

    LibSupport: Fix build on ppc32.
    When __powerpc__ is defined and __powerpc64__ is not assume ppc32.
    
    Signed-off-by: Michal Suchanek <msuchanek@suse.de>

--- src/LibSupport.h
+++ src/LibSupport.h
@@ -75,7 +75,8 @@
 #       define RADIX_BITS                      64
 #   elif defined(__i386__) || defined(__i386) || defined(i386)		\
     || defined(_WIN32) || defined(_M_IX86)				\
-    || defined(_M_ARM) || defined(__arm__) || defined(__thumb__)
+    || defined(_M_ARM) || defined(__arm__) || defined(__thumb__)	\
+    || defined(__powerpc__)
 #       define RADIX_BITS                      32
 #   else
 #       error Unable to determine RADIX_BITS from compiler environment
