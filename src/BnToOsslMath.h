ebe82ac82fef97feddc558f3c3ad16baf67e6921
Ken Goldman <kgoldman@us.ibm.com>
2024-02-27 14:57:57 -0500

    tpm: Increment supported openssl to 3.2.x
    Signed-off-by: Ken Goldman <kgoldman@us.ibm.com>

--- src/BnToOsslMath.h
+++ src/BnToOsslMath.h
@@ -77,7 +77,7 @@
 #include <openssl/ec.h>
 #include <openssl/bn.h>
 
-#if OPENSSL_VERSION_NUMBER >= 0x30100ff0L
+#if OPENSSL_VERSION_NUMBER >= 0x30200ff0L
 // Check the bignum_st definition against the one below and either update the
 // version check or provide the new definition for this version.
 #  error Untested OpenSSL version
